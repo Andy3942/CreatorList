9b595a51982aa40986f643a5f61f33199d4f5875
suifeng <suifeng@1.com>
2018-05-21 12:51:42 +0800

    下拉刷新，上拉加载更多

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -3,17 +3,18 @@ cc.Class({
 
     properties: {
         list:cc.ScrollView,
-        cell:cc.Node,
+        cell:cc.Node
     },
 
     // use this for initialization
     onLoad: function () {
+        this._cellCount = 30;
         let cellSize = this.cell.getContentSize();
         this.cell.active = false;
         var handler = function (funcName, list, index) {
             switch (funcName) {
                 case "count":
-                    return 50;
+                    return this._cellCount;
                 case "cellSize":
                     return cellSize;
                 case "cell":
@@ -25,6 +26,15 @@ cc.Class({
         }.bind(this);
         this.list.getComponent("MyList").setHandler(handler);
         this.list.getComponent("MyList").reloadData();
+        this.list.node.on("bounce-top", function(){
+            this.list.getComponent("MyList").reloadData();
+        }.bind(this));
+        this.list.node.on("bounce-bottom", function(){
+            this._cellCount += 30;
+            var curPosition = this.list.content.getPosition();
+            this.list.getComponent("MyList").reloadData();
+            this.list.content.position = curPosition;
+        }.bind(this));
     },
 
     // called every frame
